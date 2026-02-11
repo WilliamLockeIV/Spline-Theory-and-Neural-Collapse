87fa730a477d78f664f0da11a6408e73bba1cd0d
WilliamLockeIV <williamlockeiv@gmail.com>
2026-02-10 18:10:12 -0600

    Restructure .py files under "Code" folder

---
+++ Code/__init__.py
@@ -0,0 +1,9 @@
+from data import CharacterDataset
+from model_methods import (
+    fit,
+    evaluate,
+    get_activations,
+    get_neural_collapse,
+    get_local_complexity,
+    get_partitions
+)
---
+++ Code/data.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+class CharacterDataset(Dataset):
+    def __init__(self, images, labels, label_dict):
+        # Add an extra dimension for "channel" at the start of each image,
+        # so each batch of images will be of shape B,C,H,W.
+        self.images = torch.tensor(images[:,np.newaxis], dtype=torch.float)
+        self.labels = labels
+        self.class_labels = [label_dict[label] for label in labels]
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return (self.images[idx], self.class_labels[idx])
